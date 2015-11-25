1789bd8a765a2c7eee310fc1309a5e8b43e237d5
jasonkneen <jason@bouncingfish.com>
2015-11-25 17:20:46 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# installr CLI
+
+Allows you to use the basics of the installapp API from the CLI -- useful for scripting
+
+## Why?
+
+I use installrapp.com to distribute mobile apps and having already scripted my app builds, I wanted to script the installr part so I could script the entire build process, from generation of the builds to upload.
+
+I also included a feature that allowed me to do small commits in Git, and have the CLI pull these out and use them for my release notes.
+
+## Install [![NPM version](https://badge.fury.io/js/installr.svg)](http://badge.fury.io/js/installr)
+
+As global CLI:
+
+    $ npm install -g installr
+
+## Usage
+
+List all apps in installr
+```
+$ installr list --token <YOURAPITOKEN>  
+```
+## Upload an app
+```
+$ installr upload MyApp.ipa --token <YOURAPITOKEN> --notes "- Added some cool features" --emails "hello@bouncingfish.com" --teams "QA"
+```
+Suggestions, improvements, PRs, welcome!
+
+##License
+Copyright 2015 Jason Kneen
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</pre>
+
---
+++ bin/installr.js
@@ -0,0 +1,177 @@
+#!/usr/bin/env node
+
+var iTunesPath = "~/music/iTunes/iTunes\ Media/Mobile\ Applications/";
+
+var gitLog = 'git log --since=today --pretty="- %s"'
+
+var program = require('commander'),
+    chalk = require('chalk'),
+    updateNotifier = require('update-notifier'),
+    fs = require('fs'),
+    afs = require('node-appc').fs,
+    pkg = require('../package.json'),
+    exec = require('child_process').exec,
+    request = require('request'),
+    fields = require('fields'),
+    path = require('path'),
+    _ = require('underscore'),
+    Table = require('cli-table');
+
+// check if the TiApp.xml an TiCh config file exists
+if (!fs.existsSync('./tiapp.xml')) {
+    console.log(chalk.red('Please run in a Titanium project folder'));
+} else {
+    installrapp();
+}
+
+
+function listApps(params) {
+
+    console.log(chalk.yellow('Fetching apps..'));
+
+    var r = request.get({
+        url: 'https://www.installrapp.com/apps.json',
+        headers: {
+            'X-InstallrAppToken': params.token
+        }
+    }, function(err, httpResponse, body) {
+        // instantiate
+        var table = new Table({
+            head: [chalk.yellow('id'), chalk.yellow('AppId'), chalk.yellow('Platform'), chalk.yellow('Version'), chalk.yellow('buildId'), chalk.yellow('Installs')],
+            colWidths: [8, 40, 10, 15, 10, 10]
+        });
+
+        JSON.parse(body).appList.forEach(function(app) {
+            table.push(
+                [app.id, app.appId, app.type, app.latestBuild.versionNumber, app.latestBuild.id, app.latestBuild.numberInstalled]
+            );
+        });
+
+        console.log(table.toString());
+    });
+}
+
+function uploadApp(params) {
+    console.log(chalk.yellow('Uploading app to installr'));
+
+    var r = request.post({
+        url: 'https://www.installrapp.com/apps.json',
+        headers: {
+            'X-InstallrAppToken': params.token
+        }
+    }, function(err, httpResponse, body) {
+        if (err) {
+            console.log(err);
+            finished();
+        } else {
+
+            console.log(chalk.yellow('App uploaded, sending to testers..'));
+
+            var resp = JSON.parse(body);
+
+            var r = request.post({
+                url: 'https://www.installrapp.com/apps/' + resp.appData.id + '/builds/' + resp.appData.latestBuild.id + '/team.json',
+                headers: {
+                    'X-InstallrAppToken': params.token
+                }
+            }, function(err, httpResponse, body) {
+                if (err) {
+                    console.log(chalk.green(err));
+                } else {
+                    console.log(chalk.green('Sent to ' + params.emails));
+
+                }
+            });
+
+            var form = r.form();
+
+            form.append('notify', params.emails);
+
+        }
+    });
+
+
+
+    var build_file = afs.resolvePath(params.filePath);
+
+    var form = r.form();
+
+    /// specify the file
+    form.append('qqfile', fs.createReadStream(build_file));
+
+    // release notes
+    form.append('releaseNotes', params.notes);
+
+    // team names
+    if (params.teams) {
+        form.append('notify', params.teams);
+    }
+
+}
+
+// main function
+function installrapp() {
+
+    var params = {};
+
+    // setup CLI
+    program
+        .version(pkg.version, '-v, --version')
+        .usage('[options]')
+        .description(pkg.description)
+        .option('-u, --upload [location]', 'Uploads the latest build from iTunes / dist folder')
+        .option('-l, --list', 'Lists apps')
+        .option('-n, --notes <notes>', 'Release notes')
+        .option('-e, --emails <emails>', 'Comma-separated list of emails to send to')
+        .option('-t, --teams <names>', 'Comma-separated list of teams to send to')
+        .option('-c, --token <token>', 'Set the API token to use')
+
+    program.parse(process.argv);
+
+    // check for a new version
+    updateNotifier({
+        packageName: pkg.name,
+        packageVersion: pkg.version
+    }).notify();
+
+    if (program.notes) {
+        params.notes = program.notes;
+    } else {
+        exec(gitLog, function(e, out) {
+            params.notes = out;
+        });
+    }
+
+    if (program.emails) {
+        params.emails = program.emails;
+    }
+
+    if (program.teams) {
+        params.teams = program.teams;
+    }
+
+    if (program.token) {
+        params.token = program.token;
+    } else {
+        console.log("No API Token");
+        return;
+    }
+
+    if (program.upload) {
+
+        if (fs.existsSync("'" + iTunesPath.toString() + program.upload) + "'") {
+            params.filePath = iTunesPath + program.upload;
+        } else if (fs.existsSync("./dist/" + program.upload)) {
+            params.filePath = "./dist/" + program.upload;
+        } else if (fs.existsSync("./" + program.upload)) {
+            params.filePath = "./" + program.upload;
+        } else {
+            console.log(chalk.red('upload file not found!'));
+        }
+
+        uploadApp(params);
+
+    } else if (program.list) {
+        listApps(params);
+    }
+}
